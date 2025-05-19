bc16d47e4dfc66b6a6c8c7d86940bf04bd6cbc24
Prateek Mishra <pm921670@gmail.com>
2025-05-19 17:53:42 +0530

    fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGOURI, {
 
 // CORS setup for frontend communication
 app.use(cors({
-  origin: ["https://i-am-form.netlify.app", "https://odishaformbackend-1.onrender.com/api"],
+  origin: ["https://i-am-form.netlify.app", "https://odishaformbackend-1.onrender.com/api/contact"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
